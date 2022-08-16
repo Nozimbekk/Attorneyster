925463c6f2c434fad71e6009dfaf2af359e58352
Nozimbek <hamroqulovnozimbek@gmail.com>
2022-08-17 01:22:33 +0500

    finished work

--- script.js
+++ script.js
@@ -1,40 +1,22 @@
     const eightCardImg = document.querySelectorAll('.eight__card-img'),
-    eightIcons = document.querySelectorAll('.eight__icon-div'),
-    list = document.querySelector('.header__nav-item'),
-    menu = document.getElementById('menu'),
-    headerLink = document.querySelector('.header__nav-link'),
-    scrolling = document.querySelector('.header__main-thre');
+          eightIcons = document.querySelectorAll('.eight__icon-div'),
+          list = document.querySelector('.header__nav-item'),
+          menu = document.getElementById('menu'),
+          headerList = document.querySelector('.header__nav-item');
 
     menu.addEventListener('click', () => {
-        list.classList.toggle('active')
+        headerList.classList.toggle('active')
     })
-    
-    
+
     for (let i = 0; i < eightCardImg.length; i++) {
         eightCardImg[i].addEventListener('click', () =>{
         eightIcons[i].classList.add('active')
+        setTimeout(() => {
+            eightIcons[i].classList.remove('active')
+        }, 4000);
     })
-    eightCardImg[i].addEventListener('dblclick', () =>{
-        eightIcons[i].classList.remove('active')
-        
-    })
+
 }
-headerLink.addEventListener('click', function () {
-    headerLink.scrollIntoView(1000)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 AOS.init();
